641d05260aa059ac47925c85d2e7dc78eab8d022
npakudin <npakudin@hill30.com>
2016-05-06 01:16:59 +0300

    Create file from resources and pass it to TessBaseApi

--- micrtes/src/main/java/com/example/micrtes/MainActivity.java
+++ micrtes/src/main/java/com/example/micrtes/MainActivity.java
@@ -14,7 +14,11 @@ import android.widget.TextView;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,30 +26,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void recoFunc(View v){
         TextView textView = (TextView) findViewById(R.id.textView);
+        assert textView != null;
 
-//
+        String filename = "mcr.traineddata";
+        File file = new File(this.getFilesDir() + "/data/tessdata/", filename);
+        if (!file.mkdirs()) {
+            Log.wtf("MainActivity.onCreate", "Cannot mkdirs");
+        }
+
+        Log.w("MainActivity.onCreate", "filename: " + file.getAbsolutePath());
+
+        try {
+            FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            InputStream mcrInputStream = getResources().openRawResource(R.raw.mcr);
+
+            copy(mcrInputStream, outputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: compile datamodel for Android or use existing for English - I want to recognize anything for Android now
+        //
+        // Now if run app under debug (Shift + F9), then open Android Monitor (Alt + 6 - not F6, just 6)
+        // Logs looks like below. Main problem - SIGSEGV
+        //
+        // 05-06 01:02:12.100 18564-18564/com.example.micrtes W/onCreate: filename: /data/data/com.example.micrtes/files/data/tessdata/mcr.traineddata
+        // 05-06 01:02:12.110 18564-18564/com.example.micrtes V/BitmapFactory: DecodeImagePath(decodeResourceStream3) : res/drawable/nn3.png
+        // 05-06 01:02:12.170 18564-18564/com.example.micrtes E/Tesseract(native): Could not initialize Tesseract API with language=mcr!
+        // 05-06 01:02:12.410 18564-18564/com.example.micrtes A/libc: Fatal signal 11 (SIGSEGV), code 1, fault addr 0x8 in tid 18564 (example.micrtes)
+
 
         Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.nn3);
         TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.init(/+ "/mcr.traineddata", "mcr"); // myDir + "/tessdata/eng.traineddata" must be present
+        baseApi.init(this.getFilesDir() + "/data/", "mcr"); // myDir + "/tessdata/eng.traineddata" must be present
         baseApi.setImage(bm);
         String recognizedText = baseApi.getUTF8Text(); // Log or otherwise display this string...
         textView.setText(recognizedText);
         baseApi.end();
+    }
 
-
-
-
+    private void copy(InputStream inputStream, OutputStream outputStream) {
+        try {
+            byte[] buffer = new byte[1024]; // Adjust if you want
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1)
+            {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-
-
-//todo
-//написать путь до модели
-//написать путь до картинки
-
