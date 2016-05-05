3356b9a76df03ee2d8f1ff40388513bb7cec87f3
zoy <zoevever@gmail.com>
2016-05-05 12:36:59 -0500

    traineddata

--- micrtes/src/main/java/com/example/micrtes/MainActivity.java
+++ micrtes/src/main/java/com/example/micrtes/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.micrtes;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ import android.widget.TextView;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,17 +28,18 @@ public class MainActivity extends AppCompatActivity {
     public void recoFunc(View v){
         TextView textView = (TextView) findViewById(R.id.textView);
 
+//
 
-        File myDir = Environment.getRootDirectory(); //I dont have a sdcard
-        File img = new File(myDir, "nn3.png");
+        Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.nn3);
         TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.init(myDir+ "mcr.traineddata", "mcr"); // myDir + "/tessdata/eng.traineddata" must be present
-        baseApi.setImage(img);
-
+        baseApi.init(/+ "/mcr.traineddata", "mcr"); // myDir + "/tessdata/eng.traineddata" must be present
+        baseApi.setImage(bm);
         String recognizedText = baseApi.getUTF8Text(); // Log or otherwise display this string...
-        baseApi.end();
         textView.setText(recognizedText);
-//        Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.nn3);
+        baseApi.end();
+
+
+
 
     }
 }
