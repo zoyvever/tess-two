388ca035ed57efdd86d0943e9ae6c020ed386676
zoy <zoevever@gmail.com>
2016-05-08 23:06:24 -0500

    CropAndRecognize method

--- micrtes/src/main/java/com/example/micrtes/MainActivity.java
+++ micrtes/src/main/java/com/example/micrtes/MainActivity.java
@@ -3,22 +3,25 @@ package com.example.micrtes;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import fakeawt.Rectangle;
+import magick.MagickException;
+import magick.MagickImage;
+import magick.util.MagickBitmap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,31 +29,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.textView);
-        assert textView != null;
-
         String filename = "mcr.traineddata";
-
         //String base = Environment.getExternalStorageDirectory() + File.separator + "micrtes" + File.separator;
         String base = "/data/local/tmp/micr/";
-
         File file0 = new File(base);
-        if (!file0.mkdir()) {
-            Log.wtf("MainActivity.onCreate", "Cannot mkdir 0");
-        }
-        Log.wtf("MainActivity.onCreate", "file0: " + file0.exists());
-
-
         File file1 = new File(base + "tessdata");
-        if (!file1.mkdir()) {
-            Log.wtf("MainActivity.onCreate", "Cannot mkdir 1");
-        }
-
         File file = new File(base + "/tessdata/" + filename);
-        Log.w("MainActivity.onCreate", "filename: " + file.getAbsolutePath());
         try {
-            Log.wtf("MainActivity.onCreate", "file.exists: " + file.exists() + " file.canWrite: " + file.canWrite());
-
             FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
             InputStream inputStream = getResources().openRawResource(R.raw.mcr);
 
@@ -71,21 +56,11 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException ignored) {
                 }
             }
-
-
         } catch (Exception e) {
             Log.e("MainActivity", "copy", e);
         }
-        Log.wtf("MainActivity.onCreate", "file: " + file.exists() + " len : " + file.length());
 
-
-        Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.nn3);
-        TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.init(base, "mcr"); // myDir + "/tessdata/eng.traineddata" must be present
-        baseApi.setImage(bm);
-        String recognizedText = baseApi.getUTF8Text(); // Log or otherwise display this string...
-        textView.setText(recognizedText);
-        baseApi.end();
+        CropAndRecognize(base,2,3,4,5);
     }
 
     private void copy(InputStream inputStream, OutputStream outputStream) {
@@ -111,5 +86,33 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("MainActivity", "copy", e);
             }
         }
+    }
+
+    public void CropAndRecognize(String base, int a, int b, int c,int d){
+        TextView textView = (TextView) findViewById(R.id.textView);
+        assert textView != null;
+        Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.nn3);
+
+//        Rectangle bounds = new Rectangle(a,b,c,d);
+        //TODO check if bounds >0
+
+        ImageView imgview= (ImageView) findViewById(R.id.pic);
+        final MagickImage img;
+        try {
+            img = new MagickBitmap().fromBitmap(bm);
+//            img.cropImage(bounds);
+            img.thresholdImage(3);
+            imgview.setImageBitmap(MagickBitmap.ToBitmap(img));
+        } catch (MagickException e) {
+            e.printStackTrace();
+        }
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.init(base, "mcr");
+        baseApi.setImage(bm);
+        String recognizedText = baseApi.getUTF8Text();.
+        textView.setText(recognizedText);
+        baseApi.end();
+
     }
 }
