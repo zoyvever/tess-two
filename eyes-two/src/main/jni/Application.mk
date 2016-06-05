64d1ae5956336e5dfff211f0a6c6daa70ea77103
zoy <zoevever@gmail.com>
2016-06-05 13:31:07 -0500

    deleted tess-two

--- eyes-two/src/main/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI := armeabi-v7a
-APP_OPTIM := release
-NDK_TOOLCHAIN_VERSION := clang
--- tess-two/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_STL := gnustl_static
-APP_ABI := armeabi-v7a 
-APP_OPTIM := release
-APP_PLATFORM := android-9
-APP_CPPFLAGS += -fexceptions -frtti
-NDK_TOOLCHAIN_VERSION := clang
