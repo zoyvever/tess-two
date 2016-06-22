36f6182156a4868d15c5046f35d0954fccaaddb3
zoy <zoyvever@gmail.com>
2016-06-22 14:17:12 -0500

    routing number

--- TestOCR/app/src/main/java/com/example/npakudin/testocr/CheckData.java
+++ TestOCR/app/src/main/java/com/example/npakudin/testocr/CheckData.java
@@ -1,8 +1,11 @@
 package com.example.npakudin.testocr;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CheckData {
 
@@ -10,11 +13,18 @@ public class CheckData {
     public String realText = "";
     public Bitmap res;
     public String wholeText = "";
+    public String routingNumber = "";
+    //    public String accountNumber="";
     public List<TextRec.Symbol> symbols = null;
 
     public CheckData(Bitmap res, String wholeText, List<TextRec.Symbol> symbols) {
         this.res = res;
-        this.wholeText = wholeText.replaceAll("\\s", "");
+        this.wholeText = wholeText;
         this.symbols = symbols;
+        Pattern routingPattern = Pattern.compile("c\\d{9,10}|\\d{9,10}c");
+        Matcher m = routingPattern.matcher(wholeText);
+        while (m.find()) {
+            routingNumber = m.group(0).replace("c", "");
+        }
     }
 }
